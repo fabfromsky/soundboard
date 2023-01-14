79beaf14912a87b911d8fc5a353040397ff85984
cotiniaux <jcotiniaux@gmail.com>
2023-01-14 15:53:35 +0100

    add copy clipboard

--- src/components/Bouton.vue
+++ src/components/Bouton.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="sound">
+    <button style="width: 60px; border-right: 1px solid;" @click="copyURL(props.sound.id)">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+      </svg>
+    </button>
     <button @click="toggle(props.index)">
       {{ props.sound.label }}
     </button>
@@ -41,10 +47,22 @@ function stop(id) {
 function toggle(id) {
   isPlaying ? stop(id) : play(id)
 }
+
+async function copyURL(mytext) {
+  try {
+    const url = `/sound/${mytext}`
+    const imageUrl = new URL(url, import.meta.url)
+    await navigator.clipboard.writeText(imageUrl).href
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .sound {
+  background-color: white;
   display: inline-block;
   vertical-align: middle;
   margin: 30px 20px;
@@ -69,7 +87,6 @@ button {
   overflow: hidden;
   background: white;
   border: none;
-  padding-left: 20px;
 
   &:focus {
     outline: none;
@@ -108,7 +125,7 @@ button {
 audio {
   visibility: hidden;
   position: absolute;
-  top: 60px;
+  top: 65px;
   left: 0;
   width: 100%;
 
