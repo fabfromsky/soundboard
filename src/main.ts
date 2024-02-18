c9ebea2e6887190ff26b63b7ff995f1bcee8eb70
JereMiO <jcotiniaux@gmail.com>
2024-02-18 19:22:29 +0100

    chore(*): update deps

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import App from './App.vue'
 import router from './router'
-import './assets/style.css'
+import '~/assets/style.css'
 
 const app = createApp(App)
 app.use(router)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Board from '../pages/Board.vue'
+import Board from '~/pages/Board.vue'
 
 const routes = [
   {
