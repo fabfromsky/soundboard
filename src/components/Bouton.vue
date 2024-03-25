6e9919fdb8191e63b65c14d6d8786ea8c403c2ac
JereMiO <jcotiniaux@gmail.com>
2024-03-25 09:25:06 +0100

    chore(*): update deps

--- src/components/Bouton.vue
+++ src/components/Bouton.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="sound">
-    <button class="copy-cursor" style="width: 60px; border-right: 1px solid;" @click="copyURL(props.sound.src)">
+    <button class="copy-cursor" style="width: 60px; border-right: 1px solid;" @click="copyURL(sound.src)">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
         <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
         <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
       </svg>
     </button>
-    <button @click="toggle(props.sound.id.toString())">
-      {{ props.sound.label }}
+    <button @click="toggle(sound.id.toString())">
+      {{ sound.label }}
     </button>
     <div
       class="stop"
-      @click="toggle(props.sound.id.toString())"
+      @click="toggle(sound.id.toString())"
     >
       <div :class="{ 'is-playing': isPlaying }" />
     </div>
     <audio
-      :id="props.sound.id.toString()"
+      :id="sound.id.toString()"
       controls
       :class="{ 'is-playing': isPlaying }"
       @play="isPlaying = true"
       @pause="isPlaying = false"
     >
-      <source :src="`/sounds/${props.sound.src}`">
+      <source :src="`/sounds/${sound.src}`">
     </audio>
   </div>
 </template>
@@ -30,7 +30,7 @@
 <script setup lang="ts">
 import type { MySound } from '~/types/MySound'
 
-const props = defineProps<{
+defineProps<{
   sound: MySound
 }>()
 
